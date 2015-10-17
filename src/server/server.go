823a753948acb43bdb872559b62968fca116286c
Andrew Kwon <askwon@ucdavis.edu>
2015-10-17 15:01:41 -0700

    Switched from the gorilla library to golang's native websocket library and created a simple echo server

--- src/server/server.go
+++ src/server/server.go
@@ -2,56 +2,33 @@ package main
 
 import (
     // "os"
+    "io"
     "fmt" 
     "net/http"
+    // "io/ioutil"
     // "encoding/json"
     // "github.com/pkg/sftp"
-    "github.com/gorilla/websocket"
+    "golang.org/x/net/websocket"
 )
-
-var upgrader = websocket.Upgrader {
-    ReadBufferSize: 1024,
-    WriteBufferSize: 1024,
-}
 
 func printBinary(s []byte) {
     fmt.Printf("Received:")
     for i := 0; i < len(s); i++ {
-        fmt.Println("%d,", s[i])
+        fmt.Printf("%d,", s[i])
     }
     fmt.Printf("\n")
 }
 
-func handler(w http.ResponseWriter, r *http.Request) {
-    // Serve the index html file here
+func handler(socket *websocket.Conn) {
     fmt.Println("- - - - - - - - - - - - - - - - - -")
     fmt.Println("Client has connected to the server!")
 
-    // Upgrade the http server connection to the websocket protocol
-    // (Implement the request origin check later) 
-    // ^ FOLLOW THE SAME ORIGIN POLICY
-    socket, err := upgrader.Upgrade(w, r, nil)
-    if err != nil {
-        fmt.Println("Error establishing a websocket with the http session:", err)
-        return
-    }
-
-    for {
-        // Listen for messages from the client
-        messageType, p, err := socket.ReadMessage()
-        if err != nil { return }
-
-        // Print out the message
-        printBinary(p)
-
-        // Write a message to the client
-        err = socket.WriteMessage(messageType, p)
-        if err != nil { return }
-    }
+    io.Copy(socket, socket)
 }
 
 func main() {
-    http.HandleFunc("/connect", handler)
+//    http.HandleFunc("/connect", handler)
+    http.Handle("/connect", websocket.Handler(handler))
     http.Handle("/", http.FileServer(http.Dir("../../")))
-    http.ListenAndServe(":1337", nil)
+    http.ListenAndServe("localhost:1337", nil)
 }
