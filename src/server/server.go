d5fbc63d0f2e728ac89b8541f7887dbf63bf4aa7
Andrew Kwon <askwon@ucdavis.edu>
2015-10-18 23:02:53 -0700

    Globally mapped key values to specific functions to determine ui actions and resolved json parsing issues

--- src/server/server.go
+++ src/server/server.go
@@ -5,29 +5,57 @@ import (
     "net/http"
     "encoding/json"
     "golang.org/x/net/websocket"
+
+    // "github.com/askwon/Filet-Manager/sftp"
 )
 
-type UserInfo struct {
-    Hostname string
-    Port     int
-    Username string
-    Password string
+// Parse a json string into a hashmap
+func parse(jsonStr string) (map[string]string) {
+    obj := make(map[string]string)
+
+    err := json.Unmarshal([]byte(jsonStr), &obj)
+    if err != nil {
+        panic(err)
+    }
+
+    return obj
+}
+
+// Simply print contents of a json map
+func printJSON(json map[string]string) {
+    for i := range json {
+        fmt.Println(i, ":", json[i])
+    }
+}
+
+// Start the sftp connection 
+func sftpConnect(data string) {
+    auth := parse(data)
+    printJSON(auth)
+}
+
+// Map of functions to determine ui actions
+var fxns = map[string]func(data string) {
+    "sftpConnect"   : sftpConnect,
 }
 
 func handler(socket *websocket.Conn) {
     fmt.Println("- - - - - - - - - - - - - - - - - -")
     fmt.Println("Client has connected to the server!")
 
+    // Read the message that a client was connected
     var msg = make([]byte, 512) 
     _, _ = socket.Read(msg)
     fmt.Println(string(msg))
-
-    var data = make([]byte, 512) 
-    _, _ = socket.Read(data)
-    var auth UserInfo
-    err := json.Unmarshal(data, auth)
-    if err != nil {
-        fmt.Println("Error decoding json: ", err)
+	
+    for {
+	    // Receive the sftp auth information and store in a map
+        data := make(map[string]string)
+	    websocket.JSON.Receive(socket, &data)
+	    if(len(data) != 0) {
+	        fmt.Println("Received data from the client:")
+            fxns[data["key"]](data["data"])
+	    }
     }
 }
 
