05ca6cf0518c202cfb43eaa52cd6988762307958
Andrew Kwon <askwon@ucdavis.edu>
2015-11-08 17:56:00 -0800

    Updated dist files with gulp

--- dist/src/app.js
+++ dist/src/app.js
@@ -1,1 +1,9 @@
+var App = React.createClass({displayName: "App",
+    render: function() {
+        return (
+            React.createElement(Login, null)
+        )
+    }
+});
+
 ReactDOM.render(React.createElement(App, null), document.getElementById('app'));
--- dist/src/login.js
+++ dist/src/login.js
@@ -47,27 +47,30 @@ var Login = React.createClass({displayName: "Login",
     render: function() {
         var drags = {onStart: this.onStart, onStop: this.onStop};
         var {top, left} = this.state.deltaPosition; 
-        var inputStyle = {
+        var props = {
+            type        : "text",
+            className   : "login-input",
         };
         return (
             React.createElement(Draggable, React.__spread({handle: "strong"},  drags), 
                 React.createElement("div", {className: "login"}, 
-                    React.createElement("strong", {className: "menubar"}, " Login "), 
-                    React.createElement("input", {id: "hostname", type: "text", placeholder: "Hostname", 
-                        value: this.state.hostname, onKeyDown: this.handleEnterKey}), 
-                    React.createElement("input", {id: "port", type: "text", placeholder: "Port", 
-                        value: this.state.port, onKeyDown: this.handleEnterKey}), 
-                    React.createElement("input", {id: "username", type: "text", placeholder: "Username", 
-                        value: this.state.username, onKeyDown: this.handleEnterKey}), 
+                    React.createElement("strong", {className: "menubar"}, " Remote Host Login "), 
+                    React.createElement("input", React.__spread({id: "hostname", placeholder: "Hostname"},  props, 
+                        {value: this.state.hostname, onKeyDown: this.handleEnterKey})), 
+                    React.createElement("input", React.__spread({id: "port", placeholder: "Port"},  props, 
+                        {value: this.state.port, onKeyDown: this.handleEnterKey})), 
+                    React.createElement("input", React.__spread({id: "username", placeholder: "Username"},  props, 
+                        {value: this.state.username, onKeyDown: this.handleEnterKey})), 
                     React.createElement("input", {id: "password", type: "password", placeholder: "Password", 
-                        value: this.state.password, onKeyDown: this.handleEnterKey}), 
+                        value: this.state.password, onKeyDown: this.handleEnterKey, 
+                        className: "login-input"}), 
 
-                    React.createElement("button", {id: "clear-btn", type: "submit", onclick: "clean()"}, 
-						"Clear"	
-					), 
-					React.createElement("button", {id: "connect-btn", type: "submit", onclick: "connect()"}, 
-						"Connect"
-					)
+                    React.createElement("div", {id: "login-buttons"}, 
+	                    React.createElement("button", {id: "clear-btn", type: "submit", onclick: "clean()", 
+                            className: "login-button"}, " Clear "), 
+						React.createElement("button", {id: "connect-btn", type: "submit", onclick: "connect()", 
+                            className: "login-button"}, " Connect ")
+                    )
 	            )
     	    )
         );
