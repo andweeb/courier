39cdaf4bdd5b83204956722ed1f1603f89373756
Andrew Kwon <askwon@ucdavis.edu>
2015-11-14 16:36:56 -0800

    Imported sidebar component and removed Provider

--- src/scripts/App.jsx
+++ src/scripts/App.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import SideBar from './components/SideBar.jsx';
 import Login from './components/Login.jsx';
 import { Provider } from 'react-redux'
 
 // TODO: Implement store for the provider!
 ReactDOM.render(
     (
-        <Provider> 
+        <div> 
+            <SideBar/>
             <Login connId="1"/>
-        </Provider> 
+        </div> 
     ), 
     document.getElementById('app')
 );
