1c406be46442bd458c54f8cca4e441ff93b9b656
Andrew Kwon <askwon@ucdavis.edu>
2015-11-14 16:38:18 -0800

    Added a simple React sidebar component

---
+++ src/scripts/components/SideBar.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class SideBar extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            windows: ['1']
+        }
+    }   
+
+    render() {
+        return (
+            <div className="sidebar">
+
+            </div>
+        );
+    }
+};
+
+export default SideBar
