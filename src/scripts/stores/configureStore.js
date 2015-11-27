fbc5e5f613ad3fab12da9a80b49c22b1ac509e59
Andrew Kwon <askwon@ucdavis.edu>
2015-11-26 16:03:41 -0800

    Added a basic configuration store

--- src/scripts/stores/configureStore.js
+++ src/scripts/stores/configureStore.js
@@ -1,2 +1,7 @@
 import { createStore } from 'redux';
-import { rootReducer } from '../reducers/root.js
+import { rootReducer } from '../reducers';
+
+export default function configureStore(initialState) {
+    const store = createStore(rootReducer, initialState);
+    return store;
+}
