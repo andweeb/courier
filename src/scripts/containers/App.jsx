be7e0aea3d6e27a8c3207151da868127da6d6b19
Andrew Kwon <askwon@ucdavis.edu>
2015-11-26 16:03:19 -0800

    Updated inline render to class extension (move rendering to index.js later)

--- src/scripts/containers/App.jsx
+++ src/scripts/containers/App.jsx
@@ -1,16 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 import SideBar from '../components/SideBar.jsx';
 import Login from '../components/Login.jsx';
 
-// TODO: Implement store for the provider!
-ReactDOM.render(
-    (
-        <div id="container"> 
-            <SideBar/>
-            <Login connId="1"/>
-        </div> 
-    ), 
-    document.getElementById('app')
-);
+class App extends Component {
+    render() {
+        return(
+            <div id="container"> 
+                <SideBar/>
+                <Login connId="1"/>
+            </div> 
+        );
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
