38882b378bbb5eac33941e57b9827a2ac2bc47be
Andrew Kwon <askwon@ucdavis.edu>
2015-11-30 14:59:31 -0800

    Started an exported reducer for login status cases

--- src/scripts/reducers/login.js
+++ src/scripts/reducers/login.js
@@ -1,5 +1,23 @@
 import * from '../constants/ActionTypes';
 
-function requestLogin() {
-
+export function login(state = { isAttemptingLogin: false, authenticated: false }, action) {
+    switch(action.type) {
+        case LOGIN_REQUEST:
+            return Object.assign({}, state, {
+                isAttemptingLogin: true
+            });
+        case LOGIN_FAIL:
+            return Object.assign({}, state, {
+                isAttemptingLogin: false,
+                error: action.error
+                id: action.id
+            });
+        case LOGIN_SUCCESS:
+            return Object.assign({}, state, {
+                isAttemptingLogin: false,
+                authenticated: true,
+                id: action.id
+            });
+        default: return state;
+    }
 }
