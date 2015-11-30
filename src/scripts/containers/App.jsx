bbdf0b65f35e5cc6cc0ae8cc08b2d1dbeeb23dd5
Andrew Kwon <askwon@ucdavis.edu>
2015-11-30 14:58:55 -0800

    Added a function to map the state to props and exported the connection

--- src/scripts/containers/App.jsx
+++ src/scripts/containers/App.jsx
@@ -4,7 +4,23 @@ import { Provider } from 'react-redux';
 import SideBar from '../components/SideBar.jsx';
 import Login from '../components/Login.jsx';
 
+function mapStateToProps(state) {
+    const { login } = state;
+    return {
+        
+    };
+}
+
 class App extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        const { dispatch } = this.props;
+        dispatch(login)
+    }
+
     render() {
         return(
             <div id="container"> 
@@ -15,4 +31,6 @@ class App extends Component {
     }
 }
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+// ReactDOM.render(<App/>, document.getElementById('app'));
+
+export default connect(mapStateToProps)(App)
