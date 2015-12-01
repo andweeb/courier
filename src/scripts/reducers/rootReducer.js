dd6f039c9f494558e6d50c42f1071e68e7073460
Andrew Kwon <askwon@ucdavis.edu>
2015-12-01 10:21:20 -0800

    Added a reducer to return the latest action

--- src/scripts/reducers/rootReducer.js
+++ src/scripts/reducers/rootReducer.js
@@ -1,8 +1,13 @@
 import { combineReducers } from 'redux';
 import login from './login';
 
+function action(state = null, action) {
+    return action;
+}
+
 const rootReducer = combineReducers({
-    login
+    login,
+    action
 });
 
 export default rootReducer
