1be53b95e0dd73c442ed9410ff6819382b17430b
Andrew Kwon <askwon@ucdavis.edu>
2015-11-30 20:51:14 -0800

    Fixed root reducer import

--- src/scripts/stores/configureStore.js
+++ src/scripts/stores/configureStore.js
@@ -1,5 +1,5 @@
 import { createStore } from 'redux';
-import { rootReducer } from '../reducers';
+import rootReducer from '../reducers/rootReducer';
 
 export default function configureStore(initialState) {
     const store = createStore(rootReducer, initialState);
