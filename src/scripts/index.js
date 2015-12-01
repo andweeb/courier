91e76fba5df76d658e025cd3a2a8d7415ce657e6
Andrew Kwon <askwon@ucdavis.edu>
2015-11-30 21:40:04 -0800

    Added the Redux debug panel along with necessary imports

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -3,12 +3,20 @@ import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import App from './containers/App.jsx'
 import configureStore from './stores/configureStore'
+import { DevTools, DebugPanel, LogMonitor } from 'redux-devtools/lib/react';
 
 const store = configureStore();
 
 render(
-    <Provider store={store}>
-        <App/>
-    </Provider>,
+    <div>
+	    <Provider store={store}>
+	        <App/>
+	    </Provider>
+	    <DebugPanel top right bottom>
+	        <DevTools store={store}
+	                monitor={LogMonitor}
+	                visibleOnLoad={true} />
+	    </DebugPanel>
+    </div>,
     document.getElementById('app')
 )
