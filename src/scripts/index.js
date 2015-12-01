38946c5964348d12246e6115d9f99ff4ca35f332
Andrew Kwon <askwon@ucdavis.edu>
2015-11-30 20:49:15 -0800

    Fixed the imports

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import App from './containers/App'
-import configureStore from './store/configureStore'
+import App from './containers/App.jsx'
+import configureStore from './stores/configureStore'
 
 const store = configureStore();
 
