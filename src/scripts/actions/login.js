baf180ec4b2cf9ba58ee6f992fdbaf19eed528f6
Andrew Kwon <askwon@ucdavis.edu>
2015-12-01 19:53:40 -0800

    Improved names in the action functions

--- src/scripts/actions/login.js
+++ src/scripts/actions/login.js
@@ -4,22 +4,22 @@ export function loginRequest(id, credentials) {
     return { type: types.LOGIN_REQUEST, id, credentials};
 }
 
-export function loginFailure(id, message) {
-    return { type: types.LOGIN_FAIL, id, message };
-}
-
 export function loginSuccess(id, message) {
     return { type: types.LOGIN_SUCCESS, id, message };
+}
+
+export function loginFailure(id, message) {
+    return { type: types.LOGIN_FAIL, id, message };
 }
 
 export function fetchFilesRequest(id, dirpath) {
     return { type: types.FETCH_FILES_REQUEST, id, dirpath};
 }
 
-export function fetchFilesFailure(id, message) {
-    return { type: types.FETCH_FILES_FAIL, id, message };
+export function fetchFilesSuccess(id, files) {
+    return { type: types.FETCH_FILES_SUCCESS, id, files };
 }
 
-export function fetchFilesSuccess(id, files, error) {
-    return { type: types.FETCH_FILES_SUCCESS, id, error };
+export function fetchFilesFailure(id, message) {
+    return { type: types.FETCH_FILES_FAIL, id, message };
 }
