4a4cf39dd37cee192ba5d522b01a96e8e9ec6d66
Andrew Kwon <askwon@ucdavis.edu>
2015-12-16 02:31:46 -0800

    Updated initial state in root reducer

--- src/scripts/reducers/rootReducer.js
+++ src/scripts/reducers/rootReducer.js
@@ -2,8 +2,16 @@ import { combineReducers } from 'redux';
 import { handleEvent } from './login.js';
 
 const initialState = [{
-    isAttemptingLogin: false,
-    authenticated: false 
+    hostname: "",
+    port: "",
+    username: "",
+    password: "",
+    type: "",
+    message: "",
+    isAttemptingLogin: false, 
+    isAuthenticated: false,
+    opacity: 1,
+    shadow: "4px 4px 20px -1px rgba(0,0,0,0.25)",
 }];
 
 function previous(state = initialState, action) {
