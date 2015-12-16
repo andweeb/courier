382740d6a5a1cd2428da60ebbc983c89d63924e7
Andrew Kwon <askwon@ucdavis.edu>
2015-12-16 00:36:05 -0800

    Moved the event listener from actions to reducers

--- src/scripts/reducers/login.js
+++ src/scripts/reducers/login.js
@@ -20,3 +20,29 @@ const initialState = [
         authenticated: false 
     }
 ];
+
+export function handleEvent(state = initialState, event) {
+    console.log('[IN ACTIONS/LOGIN.JS] -> \nHandling event:');
+    console.dir(state);
+    console.dir(event);
+    switch (event.fxn) {
+        case LOGIN_SUCCESS:
+            return loginSuccess(event.id, event.data);
+            break;
+        case LOGIN_FAILURE:
+            console.log('[IN ACTIONS/LOGIN.JS] -> \nHandling the login failure action');
+            return loginFailure(event.id, event.data);
+            break;
+        case FETCH_FILES_SUCCESS:
+            return fetchFilesSuccess(event.id, event.data);
+            break;
+        case FETCH_FILES_FAILURE:
+            return fetchFilesFailure(event.id, event.data);
+            break;
+        default:
+            console.log('[IN ACTIONS/LOGIN.JS] -> \nUnhandled event error!');
+            return state;
+            break;
+    }
+    return;
+}
