e53609084b3d2ace6d310b4273f181f0959abaaf
Andrew Kwon <askwon@ucdavis.edu>
2015-12-25 17:25:05 -0800

    Extracted all redux-related logic to the login container component

--- src/scripts/containers/App.jsx
+++ src/scripts/containers/App.jsx
@@ -5,17 +5,53 @@ import { bindActionCreators } from 'redux'
 import SideBar from '../components/SideBar.jsx';
 import Login from '../components/Login.jsx';
 import * as LoginActions from '../actions/login';
+import InitialState from '../constants/InitialState.js';
+
+function mapStateToProps(state) {
+    return state;
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        actions: bindActionCreators(LoginActions, dispatch)
+    };
+}
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = InitialState.login;
+    }
+
+    onHandleChange(input, value) {
+        console.log('input; ');
+        console.log(input);
+        console.log('value; ');
+        console.log(value);
+        var nextState = {};
+        nextState[input] = value;
+        this.setState(nextState);
+    }
+
     render() {
-        const { login, actions } = this.props;
+        const { login, lastAction, actions } = this.props;
+        var data = {
+            connId: "1",
+            login: login,
+            lastAction: lastAction,
+            actions: actions,
+            handlers: {
+                onHandleChange: this.onHandleChange.bind(this),
+            }
+        };
         return(
             <div id="container"> 
                 <SideBar/>
-                <Login connId="1"/>
+                <Login {...data}/>
             </div> 
         );
     }
 }
 
-export default App
+// <Login connId="1" login={login} lastAction={lastAction} actions={actions}/>
+export default connect(mapStateToProps, mapDispatchToProps)(App)
