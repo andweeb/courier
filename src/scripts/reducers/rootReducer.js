75b2672820c9e39f85db84799f18487267f1ea54
Andrew Kwon <askwon@ucdavis.edu>
2015-12-25 17:25:25 -0800

    Cleaned up unecessary code

--- src/scripts/reducers/rootReducer.js
+++ src/scripts/reducers/rootReducer.js
@@ -1,9 +1,6 @@
 import { combineReducers } from 'redux';
 import { handleEvent, previous } from './login.js';
 
-console.log('handleEvent: '+handleEvent);
-console.log('previous: '+previous);
-
 const rootReducer = combineReducers({
     login: handleEvent,
     lastAction: previous
