1cac6913110bba585d19d9d6c89c5eaf2713b1e2
Andrew Kwon <askwon@ucdavis.edu>
2015-12-29 22:11:45 -0800

    Renamed action types to actions

--- src/scripts/actions/login.js
+++ src/scripts/actions/login.js
@@ -1,26 +1,26 @@
-import * as types from '../constants/ActionTypes';
+import * as actions from '../constants/ActionTypes';
 
 export function loginRequest(id, credentials) {
-    return { type: types.LOGIN_REQUEST, id, credentials};
+    return { type: actions.LOGIN_REQUEST, id, credentials};
 }
 
 export function loginSuccess(id, message) {
-    return { type: types.LOGIN_SUCCESS, id, message };
+    return { type: actions.LOGIN_SUCCESS, id, message };
 }
 
 export function loginFailure(id, message) {
-    return { type: types.LOGIN_FAILURE, id, message };
+    return { type: actions.LOGIN_FAILURE, id, message };
 }
 
 export function fetchFilesRequest(id, dirpath) {
-    return { type: types.FETCH_FILES_REQUEST, id, dirpath};
+    return { type: actions.FETCH_FILES_REQUEST, id, dirpath};
 }
 
 export function fetchFilesSuccess(id, files) {
-    return { type: types.FETCH_FILES_SUCCESS, id, files };
+    return { type: actions.FETCH_FILES_SUCCESS, id, files };
 }
 
 export function fetchFilesFailure(id, message) {
-    return { type: types.FETCH_FILES_FAILURE, id, message };
+    return { type: actions.FETCH_FILES_FAILURE, id, message };
 }
 
