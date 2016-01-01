10e3405af6104fb09a486dcf4fdedb4d4abccebc
Andrew Kwon <askwon@ucdavis.edu>
2015-12-31 21:34:48 -0800

    Added login request case in the event handler and updated cases to change isAttemptingLogin and isAuthenticated

--- src/scripts/reducers/login.js
+++ src/scripts/reducers/login.js
@@ -24,39 +24,44 @@ export function previous(state = InitialState, action) {
 export function handleEvent(state = InitialState, action) {
     console.log('[IN REDUCERS/LOGIN.JS] -> \nHandling action:');
     console.dir(action);
+
     switch (action.type) {
+        case LOGIN_REQUEST: 
+            console.log('[IN REDUCERS/LOGIN.JS] -> \nHandling the login request action');
+            return Object.assign({}, state, {
+                isAttemptingLogin: true
+            });
+
         case LOGIN_SUCCESS:
             console.log('[IN REDUCERS/LOGIN.JS] -> \nHandling the login success action');
             return Object.assign({}, state, {
-                id: action.id,
-                type: LOGIN_SUCCESS,
                 message: action.data,
-                action
+                isAttemptingLogin: false,
+                isAuthenticated: true
             });
+
         case LOGIN_FAILURE:
             console.log('[IN REDUCERS/LOGIN.JS] -> \nHandling the login failure action');
             return Object.assign({}, state, {
-                id: action.id,
-                type: LOGIN_FAILURE,
                 message: action.data,
-                action
+                isAttemptingLogin: false,
+                isAuthenticated: false
             });
+
         case FETCH_FILES_SUCCESS:
             console.log('[IN REDUCERS/LOGIN.JS] -> \nHandling the fetch files success action');
             return Object.assign({}, state, {
-                id: action.id,
                 type: FETCH_FILES_SUCCESS,
                 data: action.data,
-                action
             });
+
         case FETCH_FILES_FAILURE:
             console.log('[IN REDUCERS/LOGIN.JS] -> \nHandling the fetch files failure action');
             return Object.assign({}, state, {
-                id: action.id, 
                 type: FETCH_FILES_FAILURE,
                 data: action.data,
-                action
             });
+
         default:
             console.log(`[IN REDUCERS/LOGIN.JS] -> \nUnhandled action type error: ${action.type}`);
             return state;
