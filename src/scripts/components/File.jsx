37dbcefff4526bd77870486586c8ac0eb4d2ef2e
Andrew Kwon <askwon@ucdavis.edu>
2016-01-03 13:03:42 -0800

    Added logic for visual drag and drop cues
    Added function constants drop and canDrop in the collecting function
    fileTarget to return props necessary to render style changes depending
    on dropping.
    
    Added the endDrag function to the fileSource collecting function to
    print which file was dropped onto which file.
    
    Added injectDropProps for the DropTarget.
    
    Updated the props destructuring and implemented logic to render the
    file background color and font size depending on drag/drop state.

--- src/scripts/components/File.jsx
+++ src/scripts/components/File.jsx
@@ -1,36 +1,65 @@
 import React, { Component, PropTypes } from 'react';
-import { DragSource } from 'react-dnd';
+import { DragSource, DropTarget } from 'react-dnd';
+import flow from 'lodash/function/flow';
 
 // Implements the drag source contract
 const fileSource = {
     beginDrag(props) {
         return { filename: props.filename };
+    },
+
+    endDrag(props, monitor) {
+        const item = monitor.getItem();
+        const dropResult = monitor.getDropResult();
+
+        if(dropResult) {
+            console.log(`You dropped ${item.filename} into ${dropResult.filename}!`);
+            console.dir(dropResult);
+        }
     }
 };
 
+const fileTarget = {
+    drop(props) {
+        return { filename: props.filename };
+    },
+
+    canDrop(props) {
+        return props.isDir;
+    }
+};
+
+function injectDropProps(connect, monitor) {
+    return { 
+        connectDropTarget: connect.dropTarget(),
+        isOver: monitor.isOver(),
+        canDrop: monitor.canDrop()
+    };
+}
+
 // Specifies props to inject into the component
-function injectProps(connect, monitor) {
+function injectDragProps(connect, monitor) {
     return {
         connectDragSource: connect.dragSource(),
         isDragging: monitor.isDragging(),
     };
 }
 
-const propTypes = {
-    filename: PropTypes.string.isRequired,
-    isDragging: PropTypes.bool.isRequired,
-    connectDragSource: PropTypes.func.isRequired
-};
-
 class File extends Component {
     render() {
-        const { isDragging, connectDragSource, filename } = this.props;
-        let dragging = isDragging ? "I AM BEING DRAGGED" : "DONT TOUCH ME";
-        return connectDragSource( 
-            <li className="file">{filename}</li>
-        );
+        const { isDragging, isOver, canDrop, connectDropTarget, filename, connectDragSource } = this.props;
+        const style = {
+            fontSize: isDragging ? '14px' : '',
+            backgroundColor: canDrop && isOver ? 'rgb(207, 241, 252)' : 'transparent'
+        }
+
+        return connectDragSource(connectDropTarget(
+            <li className="file" style={style}> {filename} </li>
+        ));
     }
 };
 
-File.propTypes = propTypes;
-export default DragSource('file', fileSource, injectProps)(File);
+export default flow(
+    DropTarget('file', fileTarget, injectDropProps),
+    DragSource('file', fileSource, injectDragProps)
+)(File);
