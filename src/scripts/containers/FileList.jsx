762ba0c011312e1e7e48aeab06348414d119168c
Andrew Kwon <askwon@ucdavis.edu>
2016-01-03 13:09:25 -0800

    Added propTypes and passed in isDir to each File

--- src/scripts/containers/FileList.jsx
+++ src/scripts/containers/FileList.jsx
@@ -4,13 +4,18 @@ import { DragDropContext } from 'react-dnd';
 import File from '../components/File.jsx';
 
 class FileList extends Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <ul className="bulletless">
-                { this.props.files.map((file, i) => <File key={i} filename={file.Filename}/> )}
+                { this.props.files.map((file, i) => <File key={i} filename={file.Filename} isDir={file.IsDir}/> )}
             </ul>
         );
     }
 };
 
+FileList.propTypes = { files: PropTypes.array.isRequired };
 export default DragDropContext(HTML5Backend)(FileList);
