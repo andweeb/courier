65c26710b9ad16d10bd976ae06058af3e4dc412d
Andrew Kwon <askwon@ucdavis.edu>
2016-01-04 19:57:52 -0800

    Added a file event handler reducer for the file select and unselect actions

---
+++ src/scripts/reducers/file.js
@@ -0,0 +1,28 @@
+import { StoreInitialState } from '../constants/InitialStates.js';
+import { FILE_SELECTED, FILE_UNSELECTED } from '../constants/ActionTypes.js';
+
+export function handleFileEvent(state = StoreInitialState, action) {
+    console.log('[IN REDUCERS/FILE.JS] -> \nHandling action:');
+    console.dir(action);
+    console.log("FILE STATE: ");
+    console.dir(state);
+
+    switch (action.type) {
+        case FILE_SELECTED:
+            console.log('[IN REDUCERS/FILES.JS] -> \nHandling the file selected action');
+            state.selected.push(action.file);
+            return state;
+
+        case FILE_UNSELECTED:
+            console.log('[IN REDUCERS/FILES.JS] -> \nHandling the file selected action');
+            state.selected.map((file, i) => { 
+                if(file.Filename === action.file.Filename) 
+                    state.selected.splice(i, 1);
+            });
+            return state;
+
+        default:
+            console.log(`[IN REDUCERS/FILE.JS] -> \nUnhandled action type error: ${action.type}`);
+            return state;
+    }
+}
