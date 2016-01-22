dd1521bddeb5e150a461ba9f80758814fbcb6648
Andrew Kwon <askwon@ucdavis.edu>
2016-01-21 22:52:17 -0800

    Fixed the select state mutations with the update addon

--- src/scripts/reducers/file.js
+++ src/scripts/reducers/file.js
@@ -1,27 +1,38 @@
+import update from 'react-addons-update';
 import { StoreInitialState } from '../constants/InitialStates.js';
-import { FILE_SELECTED, FILE_SELECTED_META, FILE_DESELECTED } from '../constants/ActionTypes.js';
+import { FILE_SELECTED, FILE_GROUP_SELECTED, FILE_DESELECTED } from '../constants/ActionTypes.js';
 
 export function handleFileEvent(state = StoreInitialState, action) {
     console.log('[IN REDUCERS/FILE.JS] -> \nHandling action:');
     console.dir(action);
     console.log("FILE STATE: ");
     console.dir(state);
+    let newState = Object.assign({}, state);
 
     switch (action.type) {
         case FILE_SELECTED:
             console.log('[IN REDUCERS/FILES.JS] -> \nHandling the file selected action');
-            state.selected = { [action.file.Filename] : action.file };
-            return state;
+            newState = Object.assign({}, state);
+            newState.selected = {};
+            return update(newState, {
+                selected: {
+                    [action.file.Filename]: { $set: action.file }
+                }
+            });
 
-        case FILE_SELECTED_META:
-            console.log('[IN REDUCERS/FILES.JS] -> \nHandling the file selected action');
-            state.selected[action.file.Filename] = action.file;
-            return state;
+        case FILE_GROUP_SELECTED:
+            console.log('[IN REDUCERS/FILES.JS] -> \nHandling the file group selected action');
+            return update(state, {
+                selected: {
+                    [action.file.Filename]: { $set: action.file }
+                }
+            });
 
         case FILE_DESELECTED:
-            console.log('[IN REDUCERS/FILES.JS] -> \nHandling the file selected action');
-            delete state.selected[action.file.Filename];
-            return state;
+            console.log('[IN REDUCERS/FILES.JS] -> \nHandling the file deselected action');
+            newState.selected = Object.assign({}, state.selected);
+            delete newState.selected[action.file.Filename];
+            return newState;
 
         default:
             console.log(`[IN REDUCERS/FILE.JS] -> \nUnhandled action type error: ${action.type}`);
