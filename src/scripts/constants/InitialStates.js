32d7907e3e3a438c5fb03a9cb468727bb5e49d71
Andrew Kwon <askwon@ucdavis.edu>
2016-01-23 00:53:26 -0800

    Added App state to Login and added temp App state

--- src/scripts/constants/InitialStates.js
+++ src/scripts/constants/InitialStates.js
@@ -18,14 +18,15 @@ const StoreInitialState = {
 };
 
 const AppInitialState = {
-    hostname: "",
-    port    : "",
-    username: "",
-    password: ""
+    windows: [0, 1, 2]
 };
 
 const LoginInitialState = {
     opacity: 1,
+    hostname: "",
+    port    : "",
+    username: "",
+    password: "",
     shadow: "4px 4px 20px -1px rgba(0,0,0,0.25)"
 };
 
