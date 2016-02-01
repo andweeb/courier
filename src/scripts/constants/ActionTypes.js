49c3fb2942be786bb2557b40998e36d706dab408
Andrew Kwon <askwon@ucdavis.edu>
2016-01-31 22:37:35 -0800

    Added window focused action constant

--- src/scripts/constants/ActionTypes.js
+++ src/scripts/constants/ActionTypes.js
@@ -1,3 +1,5 @@
+export const WINDOW_FOCUSED = 'WINDOW_FOCUSED';
+
 export const LOGIN_REQUEST = 'LOGIN_REQUEST';
 export const LOGIN_FAILURE = 'LOGIN_FAILURE';
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS';
